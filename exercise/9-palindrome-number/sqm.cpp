f94e3aacc42c162b9857da1898b26c431936ce85
Mr.Hope <zhangbowang1998@gmail.com>
2020-11-08 17:27:09 +0800

    fix: solve readme conflict and format cpp files

--- exercise/9-palindrome-number/sqm.cpp
+++ exercise/9-palindrome-number/sqm.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
-#include<string>
+#include <algorithm>
+#include <iostream>
+#include <string>
 using namespace std;
+
 class Solution {
-public:
-    bool isPalindrome(int x) {
-        string tmp=to_string(x);
-        string tmp2=tmp;
-        reverse(tmp2.begin(),tmp2.end());
-        if(tmp==tmp2) return true;
-        return false;
-    }
+ public:
+  bool isPalindrome(int x) {
+    string tmp = to_string(x);
+    string tmp2 = tmp;
+    reverse(tmp2.begin(), tmp2.end());
+    if (tmp == tmp2) return true;
+    return false;
+  }
 };
