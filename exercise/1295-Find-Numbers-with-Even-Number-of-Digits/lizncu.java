8afed7c1a5cdb7b3fa5fcf929648daca8a486857
lizncu0817 <790335504@qq.com>
2020-11-24 09:07:46 +0800

    feat(lizncu): add exercise

---
+++ exercise/1295-Find-Numbers-with-Even-Number-of-Digits/lizncu.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+
+        int ans = 0;
+
+        for(int i = 0; i < nums.length;i++){
+
+            int count = 0;
+            if(nums[i] == 0){
+                continue;
+            }
+            else{
+                while(nums[i] != 0){
+
+                    nums[i] /= 10;
+                    count++;
+
+                }
+            }
+            if(count % 2 == 0){
+                ans++;
+            }
+
+        }
+
+        return ans;
+
+    }
+}
