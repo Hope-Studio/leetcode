84eef0e5007e65ff667817f9b1e851dbaa7993d3
sqm <71817526+1512825845@users.noreply.github.com>
2020-11-25 18:47:21 +0800

    feat: add Sqm (#35)

---
+++ exercise/66-plus-one/sqm.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        digits[digits.size()-1]+=1;
+        for(int i=digits.size()-1;i>0;i--)
+        {
+            if(digits[i]==10)
+            {
+                digits[i-1]++;
+                digits[i]=0;
+            }
+        }
+        if(digits[0]==10)
+        {
+            digits[0]=0;
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
+
+    }
+};
