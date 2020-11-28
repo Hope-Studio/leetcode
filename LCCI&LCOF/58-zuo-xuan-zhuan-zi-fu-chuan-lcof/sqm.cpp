ffba69260c45210610327823e253e21b8fd13d8b
sqm <71817526+1512825845@users.noreply.github.com>
2020-11-28 19:33:23 +0800

    feat: add Sqm (#41)
    * feat: add Sqm
    
    * feat: add Sqm
    
    * feat: add Sqm
    
    * feat: add Sqm

---
+++ LCCI&LCOF/58-zuo-xuan-zhuan-zi-fu-chuan-lcof/sqm.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    string reverseLeftWords(string s, int n) {
+        reverse(s.begin(),s.begin()+n);
+        reverse(s.begin()+n,s.end());
+        reverse(s.begin(),s.end());
+        
+        return s;
+
+    }
+};
---
+++ exercise/237-Delete-Node-in-a-Linked-List/sqm.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+struct ListNode
+{
+    /* data */
+    int val;
+    ListNode* next;
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val=node->next->val;
+        node->next=node->next->next;
+        
+    }
+};
