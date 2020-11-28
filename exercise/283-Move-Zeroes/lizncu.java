20eabf02552d58ab480befedc90dfb3adecdef73
lizncu0817 <790335504@qq.com>
2020-11-28 09:32:11 +0800

    feat(lizncu): add exercise

---
+++ exercise/283-Move-Zeroes/lizncu.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        for(int i=0,count=0;i<nums.length;i++){
+            
+            if(nums[i] != 0){
+                
+                if(count != i){
+                    nums[count] = nums[i];
+                    nums[i] = 0;
+                }
+               
+                count++;
+            }
+        }
+        
+    }
+}
